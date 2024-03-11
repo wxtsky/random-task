200f8417626435e7a8bb2a575fd9ce8212937230
wxtsky <1970550145@qq.com>
2024-03-12 00:06:05 +0800

    优化

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,22 @@
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 import "./globals.css";
+import {AntdRegistry} from "@ant-design/nextjs-registry";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata = {
-  title: "RandomTask",
-  description: "RandomTask",
+    title: "RandomTask",
+    description: "RandomTask",
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <AntdRegistry>
+            {children}
+        </AntdRegistry>
+        </body>
+        </html>
+    );
 }
